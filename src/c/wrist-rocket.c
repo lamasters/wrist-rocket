3a3be0fb2f50d9618c71e2f68e9a6315fbc7ee22
lamasters <lamasters@protonmail.com>
2025-03-03 18:51:25 -0500

    Add configuration for weather display

--- src/c/wrist-rocket.c
+++ src/c/wrist-rocket.c
@@ -4,17 +4,46 @@ static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_countdown_layer;
 static TextLayer *s_name_layer;
+static TextLayer *s_weather_layer;
 static BitmapLayer *s_rocket_layer;
 static GBitmap *s_rocket_bitmap;
+static BitmapLayer *s_weather_icon_layer;
+static GBitmap *s_weather_bitmap;
+
 static char s_countdown_buffer[64];
 static char s_rocket_buffer[128];
+static char s_weather_buffer[128];
+
 static int32_t s_minutes_to_launch = 0;
 static bool first_update;
+static bool weather_enabled = false;
+static char *weather_units = "";
+static int weather_icon = 4;
+
+static int32_t WEATHER_ICONS[6] = {
+  RESOURCE_ID_LIGHTNING,
+  RESOURCE_ID_RAIN,
+  RESOURCE_ID_SNOW,
+  RESOURCE_ID_FOG,
+  RESOURCE_ID_CLEAR,
+  RESOURCE_ID_CLOUDS,
+};
+
+static void request_weather_data()
+{
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  dict_write_cstring(iter, MESSAGE_KEY_MESSAGE_TYPE, "weather");
+  app_message_outbox_send();
+}
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context)
 {
   Tuple *countdown_tuple = dict_find(iterator, MESSAGE_KEY_MINUTES_TO_LAUNCH);
   Tuple *rocket_tuple = dict_find(iterator, MESSAGE_KEY_ROCKET);
+  Tuple *temperature_tuple = dict_find(iterator, MESSAGE_KEY_TEMPERATURE);
+  Tuple *conditions_tuple = dict_find(iterator, MESSAGE_KEY_CONDITIONS);
+  Tuple *weather_units_tuple = dict_find(iterator, MESSAGE_KEY_UNITS);
 
   if (countdown_tuple)
   {
@@ -32,6 +61,41 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   {
     snprintf(s_rocket_buffer, sizeof(s_rocket_buffer), "%s", rocket_tuple->value->cstring);
     text_layer_set_text(s_name_layer, s_rocket_buffer);
+  }
+
+  if (weather_units_tuple) {
+    if (strcmp(weather_units_tuple->value->cstring, weather_units) != 0)
+    {
+      weather_units = strncpy(weather_units, weather_units_tuple->value->cstring, sizeof(weather_units) - 1);
+      weather_units[sizeof(weather_units) - 1] = '\0';
+      request_weather_data();
+    }
+  }
+  
+  if (temperature_tuple && conditions_tuple)
+  {
+    if (!weather_enabled) {
+      weather_enabled = true;
+      layer_set_hidden(text_layer_get_layer(s_weather_layer), false);
+      layer_set_hidden(bitmap_layer_get_layer(s_weather_icon_layer), false);
+      bitmap_layer_destroy(s_rocket_layer);
+      s_rocket_layer = bitmap_layer_create(
+          GRect(PBL_IF_ROUND_ELSE(40, 25), PBL_IF_ROUND_ELSE(55, 50), 40, 60));
+      bitmap_layer_set_bitmap(s_rocket_layer, s_rocket_bitmap);
+      Layer *window_layer = window_get_root_layer(s_main_window);
+      layer_add_child(window_layer, bitmap_layer_get_layer(s_rocket_layer));
+    }
+    int temperature = temperature_tuple->value->int32;
+    snprintf(s_weather_buffer, sizeof(s_weather_buffer), "%d °%s", temperature, weather_units);
+    text_layer_set_text(s_weather_layer, s_weather_buffer);
+    if (conditions_tuple->value->int32 != weather_icon) {
+      APP_LOG(APP_LOG_LEVEL_INFO, "Updating weather icon");
+      weather_icon = conditions_tuple->value->int32;
+      gbitmap_destroy(s_weather_bitmap);
+      s_weather_bitmap = gbitmap_create_with_resource(WEATHER_ICONS[weather_icon]);
+      bitmap_layer_set_bitmap(s_weather_icon_layer, s_weather_bitmap);
+      layer_mark_dirty(bitmap_layer_get_layer(s_weather_icon_layer));
+    }
   }
 }
 
@@ -63,11 +127,14 @@ static void update_time()
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
+  if (tick_time->tm_min % 60 == 0 && weather_enabled) {
+    request_weather_data();
+  }
   if (tick_time->tm_min % 15 == 0 || s_minutes_to_launch < 10)
   {
     DictionaryIterator *iter;
     app_message_outbox_begin(&iter);
-    dict_write_uint8(iter, 0, 0);
+    dict_write_cstring(iter, MESSAGE_KEY_MESSAGE_TYPE, "rocket");
     app_message_outbox_send();
   }
   else if (s_minutes_to_launch > 0 && !first_update)
@@ -89,8 +156,10 @@ static void main_window_load(Window *window)
 
   // Draw the rocket image
   s_rocket_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ROCKET);
-  s_rocket_layer = bitmap_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(55, 50), bounds.size.w, 60));
+  if (!weather_enabled) {
+    s_rocket_layer = bitmap_layer_create(
+        GRect(0, PBL_IF_ROUND_ELSE(55, 50), bounds.size.w, 60));
+  }
   bitmap_layer_set_bitmap(s_rocket_layer, s_rocket_bitmap);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_rocket_layer));
 
@@ -122,6 +191,30 @@ static void main_window_load(Window *window)
   text_layer_set_text_alignment(s_name_layer, GTextAlignmentCenter);
   text_layer_set_text(s_name_layer, "Loading...");
   layer_add_child(window_layer, text_layer_get_layer(s_name_layer));
+
+  // Draw the temperature
+  s_weather_layer = text_layer_create(
+    GRect(PBL_IF_ROUND_ELSE(85, 70), PBL_IF_ROUND_ELSE(55, 50), 60, 50));
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_text_color(s_weather_layer, GColorWhite);
+  text_layer_set_font(s_weather_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_weather_layer, "");
+  layer_add_child(window_layer, text_layer_get_layer(s_weather_layer));
+
+  // Draw the weather conditions icon;
+  s_weather_icon_layer = bitmap_layer_create(
+      GRect(PBL_IF_ROUND_ELSE(85, 70), PBL_IF_ROUND_ELSE(70, 65), 60, 60));
+  s_weather_bitmap = gbitmap_create_with_resource(WEATHER_ICONS[weather_icon]);
+  bitmap_layer_set_bitmap(s_weather_icon_layer, s_weather_bitmap);
+  bitmap_layer_set_alignment(s_weather_icon_layer, GAlignCenter);
+  bitmap_layer_set_compositing_mode(s_weather_icon_layer, GCompOpSet);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_weather_icon_layer));
+
+  if (!weather_enabled) {
+    layer_set_hidden(text_layer_get_layer(s_weather_layer), true);
+    layer_set_hidden(bitmap_layer_get_layer(s_weather_icon_layer), true);
+  }
 }
 
 static void main_window_unload(Window *window)
@@ -129,8 +222,11 @@ static void main_window_unload(Window *window)
   text_layer_destroy(s_name_layer);
   text_layer_destroy(s_countdown_layer);
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_weather_layer);
   gbitmap_destroy(s_rocket_bitmap);
   bitmap_layer_destroy(s_rocket_layer);
+  gbitmap_destroy(s_weather_bitmap);
+  bitmap_layer_destroy(s_weather_icon_layer);
 }
 
 static void init()
--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,3 +1,74 @@
+var Clay = require("pebble-clay");
+var clayConfig = require("./config.json");
+var clay = new Clay(clayConfig);
+
+var CODE_PREFIX_TO_ICON_INDEX = {
+  2: 0,
+  3: 1,
+  5: 1,
+  6: 2,
+  7: 3,
+  8: 4,
+};
+
+function weatherIdToIconIndex(weatherId) {
+  if (weatherId > 800) {
+    return 5;
+  }
+  return CODE_PREFIX_TO_ICON_INDEX[Math.floor(weatherId / 100)];
+}
+
+function getWeatherData() {
+  var settings = JSON.parse(localStorage.getItem("clay-settings")) || {};
+  var owm_key = settings.OWM_API_KEY;
+
+  if (!owm_key) {
+    return;
+  }
+  var units = settings.UNITS === "F" ? "imperial" : "metric";
+  navigator.geolocation.getCurrentPosition(
+    function (pos) {
+      var xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        var response = JSON.parse(xhr.responseText);
+        var temperature = Math.round(response.main.temp);
+        var conditions = weatherIdToIconIndex(response.weather[0].id);
+        Pebble.sendAppMessage(
+          {
+            UNITS: settings.UNITS || "C",
+            TEMPERATURE: temperature,
+            CONDITIONS: conditions,
+          },
+          function (e) {
+            console.log("Weather info sent to Pebble successfully!");
+          },
+          function (e) {
+            console.log("Error sending weather info to Pebble!");
+          }
+        );
+      };
+      var owm_url =
+        "https://api.openweathermap.org/data/2.5/weather?lat=" +
+        pos.coords.latitude +
+        "&lon=" +
+        pos.coords.longitude +
+        "&appid=" +
+        owm_key +
+        "&units=" +
+        units;
+      xhr.open("GET", owm_url);
+      xhr.send();
+    },
+    function (err) {
+      console.log("Error requesting location!");
+    },
+    {
+      timeout: 15000,
+      maximumAge: 60000,
+    }
+  );
+}
+
 function getLaunchData() {
   var xhr = new XMLHttpRequest();
   xhr.onload = function () {
@@ -36,8 +107,13 @@ function getLaunchData() {
 
 Pebble.addEventListener("ready", function () {
   getLaunchData();
+  getWeatherData();
 });
 
 Pebble.addEventListener("appmessage", function (e) {
-  getLaunchData();
+  if (e.payload.MESSAGE_TYPE === "weather") {
+    getWeatherData();
+  } else {
+    getLaunchData();
+  }
 });
